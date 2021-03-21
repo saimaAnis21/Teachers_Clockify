c33d335607936611b3514cc215b90c507747df71
saimaAnis21 <saima_rehman21@yahoo.com>
2021-03-22 01:30:30 +0500

    Update new group form

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 5 }
-  validates :name, format: { with: /\A'Grade-'\d\Z/, message: "Correct format for writing name is 'Grade-x' replace x with the desired grade level "}
+  validates :name, format: { with: /\AGrade\-\b([1-9]|1[0-2])\b\Z/, message: "Correct format for writing name is 'Grade-x' replace x with the desired grade level "}
   validates_uniqueness_of :name, on: :create, message: "This group name is already taken!"
 
   belongs_to :user
