6d9b1e8e8b8b07ba7efdb04cb028d51192dee4e1
saimaAnis21 <saima_rehman21@yahoo.com>
2021-03-24 00:50:39 +0500

    Ypdate helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 # rubocop:disable Metrics/PerceivedComplexity
 # rubocop:disable Metrics/CyclomaticComplexity
 module ApplicationHelper
+
+  def time_in_hrs(mins)
+    hrs = (mins / 60).to_s.concat(' hrs ')
+    hrs.concat((mins % 60).to_s.concat(' mins ')) if mins % 60
+    hrs
+  end
+  
   def show_anw
     if flash[:alert]
       render(partial: 'shared/alert')
@@ -13,8 +20,8 @@ module ApplicationHelper
 
   def icon_dis
     content = ''
-    if logged_in? && !request.url.include?('user/show')
-      content << "<span> #{link_to fa_icon('align-justify'), user_show_path, class: 'fa-3x color-green m-10'} </span>"
+    if !request.url.include?('user')
+      content << "<span> #{link_to fa_icon('align-justify'), user_path, class: 'fa-3x color-green m-10'} </span>"
 
     end
     content.html_safe
@@ -24,17 +31,17 @@ module ApplicationHelper
     content = ''
     content << "<div class='pr-10'>
                       #{ if logged_in?
-                           link_to 'Logout', session_logout_path, method: :delete
+                           link_to 'Logout', session_path, method: :delete
                          else
-                           link_to 'Log In', session_new_path
+                           link_to 'Log In', new_session_path
                          end }
                       </div>
                       <div class='vl pr-10'></div>
                       <div>
                       #{ if logged_in?
-                           link_to current_user.name, user_show_path
+                           link_to current_user.name, user_path
                          else
-                           link_to 'Register', user_new_path
+                           link_to 'Register', new_user_path
                          end }
                       </div>"
     content.html_safe
--- app/helpers/time_spent_helper.rb
+++ app/helpers/time_spent_helper.rb
@@ -1,7 +1,23 @@
 module TimeSpentHelper
-  def time_in_hrs(mins)
-    hrs = (mins / 60).to_s.concat(' hrs ')
-    hrs.concat((mins % 60).to_s.concat(' mins ')) if mins % 60
-    hrs
+  
+
+  def show_timespent
+    content = ''
+
+    if !@ts.nil?
+      content << "<div class='d-inline'>#{ @ts.created_at.strftime('%d %b %Y') }</div>
+      <div class='d-inline pl-5'>#{ @ts.name }</div>
+      <div class='float-r'>Total:#{ time_in_hrs(@ts.Amount)} </div>"
+     end 
+     content.html_safe
   end
+
+  def render_grpview
+    content=''
+        if !@ts.nil?
+          content << "<div> #{ render 'shared/groupview' } </div>"
+        end
+    content.html_safe
+  end
+
 end
