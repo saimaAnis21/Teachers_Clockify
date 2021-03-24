b202ca157225c41a8ff226244eda18474d1ea065
saimaAnis21 <saima_rehman21@yahoo.com>
2021-03-24 19:03:33 +0500

    Remove commented code

--- app/controllers/time_spent_controller.rb
+++ app/controllers/time_spent_controller.rb
@@ -31,34 +31,12 @@ class TimeSpentController < ApplicationController
     @ts2 = TimeSpent.new
   end
 
-  # def create2
-  #   amt = params[:loghrs][:amount].to_i
-  #   @ts2 = current_user.time_spents.build(name: params[:loghrs][:name], Amount: amt)
-  #   respond_to do |format|
-  #     if @ts2.save
-
-  #       format.html { redirect_to time_spent_plan_check_path, notice: 'Entry successfully created!!' }
-  #     elsif @ts2.errors.any?
-  #       format.html { redirect_to time_spent_plan_check_new_path, alert: @ts2.errors.full_messages }
-  #     else
-  #       format.html { redirect_to time_spent_plan_check_new_path, alert: 'Entry not created!!' }
-
-  #     end
-  #   end
-  # end
-
   def show
-    # t = TimeSpent.where(author_id: session[:current_user_id]).all
-    # ts_arr = GroupTime.select('time_spent_id').distinct.map(&:time_spent_id)
-    # @ts = t.where('id IN (?)', Array.wrap(ts_arr)).order(:created_at).last
     @ts = current_user.time_spents.where('id IN (?)', GroupTime.select('time_spent_id')).last
     @grp = @ts.groups unless @ts.nil?
   end
 
   def plancheckshow
-    # t = TimeSpent.where(author_id: session[:current_user_id]).all
-    # ts_arr = GroupTime.select('time_spent_id').distinct.map(&:time_spent_id)
-    # @ts = t.where('id NOT IN (?)', Array.wrap(ts_arr)).order(created_at: :desc)
     @ts = current_user.time_spents.where('id NOT IN (?)', GroupTime.select('time_spent_id')).order(created_at: :desc)
   end
 
